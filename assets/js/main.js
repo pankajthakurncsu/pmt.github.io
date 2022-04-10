93bbb2b95242cd42890b5d0b0f71f9fcf5339007
Dnyanesh Kolhe <61074868+dnyanesh2408@users.noreply.github.com>
2022-04-10 10:29:27 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,12 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+$(document).ready(function() {
+  setTimeout(()=>{
+      $('#Preloader').addClass('loader');
+  },1500);
+  setTimeout(()=>{
+      $('#Preloader').remove('loader');
+  },4000);
+})
