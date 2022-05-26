c5b0f0ef098e6d80240533cb11ba2234bf3a92b6
Dnyanesh Kolhe <61074868+dnyanesh2408@users.noreply.github.com>
2022-05-27 02:39:47 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,84 @@
-var myVar;
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
 
-function myFunction() {
-  myVar = setTimeout(showPage, 1000);
+menu.onclick = () => {
+    navbar.classList.toggle('open-menu');
+    menu.classList.toggle("move");
+};
+window.onscroll = () => {
+    navbar.classList.remove("open-menu");
+    menu.classList.remove("move");
 }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+
+// Email Js
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == ""){
+            emptyerror();
+        } else {
+            sendmail (name.value, email.value, msg.value);
+            success();
+        }
+    });
+}
+validate();
+
+function sendmail(name,email,msg){
+    emailjs.send("service_m8bonqe","template_wtg6f5w",{
+        from_name: email,
+        to_name: name,
+        message: msg,
+        });
 }
 
+function emptyerror() {
+    swal({
+        title: "Oh No....!",
+        text: "Fields can not be empty!",
+        icon: "error",
+        // icon: "success",
+        // button: "error!",
+      });
+}
+
+function success() {
+    swal({
+        title: "Email sent sucessfully!",
+        text: "We try to replay in 24 hours!",
+        icon: "success",
+      });
+}
+
+// Header Background Change on Scrool
+let Header = document.querySelector(".header")
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-active', window.scrollY > 0);
+});
+
+// Scroll Top
+
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+    scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
+});
+
+
+
+// preloader
 $(document).ready(function() {
-  setTimeout(()=>{
-      $('#Preloader').addClass('loader');
-  },1500);
-  setTimeout(()=>{
-      $('#Preloader').remove('loader');
-  },4000);
-})
+    setTimeout(()=>{
+        $('#Preloader').addClass('loader');
+    },1000);
+    setTimeout(()=>{
+        $('#Preloader').remove('loader');
+    },500);
+  })
