06dd79008f1a61d8db905ccbd0d4840ed2e4d3b7
Dnyanesh Kolhe <61074868+dnyanesh2408@users.noreply.github.com>
2022-10-02 23:32:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,110 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    navbar.classList.toggle('open-menu');
-    menu.classList.toggle("move");
-};
-window.onscroll = () => {
-    navbar.classList.remove("open-menu");
-    menu.classList.remove("move");
+// Script for when click nav toggler outside on screen then navbar collapse
+window.onload = function () {
+	document.addEventListener("click", function (event) {
+		// if the clicked element isn't child of the navbar, you must close it if is open
+		if (!event.target.closest("#navbar") && document.getElementById("navbarNav").classList.contains("show")) {
+			document.getElementById("menu_button").click();
+		}
+	});
 }
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// Back to Top Button
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// End Back to Top Button
 
 
 // Email Js
 function validate() {
-    let name = document.querySelector(".name");
-    let email = document.querySelector(".email");
-    let msg = document.querySelector(".message");
-    let sendBtn = document.querySelector(".send-btn");
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
 
-    sendBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (name.value == "" || email.value == "" || msg.value == ""){
-            emptyerror();
-        } else {
-            sendmail (name.value, email.value, msg.value);
-            success();
-        }
-    });
+  sendBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (name.value == "" || email.value == "" || msg.value == ""){
+          emptyerror();
+      } else {
+          sendmail (name.value, email.value, msg.value);
+          success();
+      }
+  });
 }
 validate();
 
 function sendmail(name,email,msg){
-    emailjs.send("service_m8bonqe","template_wtg6f5w",{
-        from_name: email,
-        to_name: name,
-        message: msg,
-        });
+  emailjs.send("service_m8bonqe","template_wtg6f5w",{
+      from_name: email,
+      to_name: name,
+      message: msg,
+      });
 }
 
 function emptyerror() {
-    swal({
-        title: "Oh No....!",
-        text: "Fields can not be empty!",
-        icon: "error",
-        // icon: "success",
-        // button: "error!",
-      });
+  swal({
+      title: "Oh No....!",
+      text: "Fields can not be empty!",
+      icon: "error",
+      // icon: "success",
+      // button: "error!",
+    });
 }
 
 function success() {
-    swal({
-        title: "Email sent sucessfully!",
-        text: "We try to replay in 24 hours!",
-        icon: "success",
-      });
+  swal({
+      title: "Email sent sucessfully!",
+      // text: "We try to replay in 24 hours!",
+  text: "Thanks for connecting, I try to replay asap!",
+      icon: "success",
+    });
 }
 
-// Header Background Change on Scrool
-let Header = document.querySelector(".header")
-
-window.addEventListener('scroll', () => {
-    header.classList.toggle('header-active', window.scrollY > 0);
-});
-
-// Scroll Top
-
-let scrollTop = document.querySelector(".scroll-top");
-
-window.addEventListener("scroll", () => {
-    scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
-});
-
-
+// // preloader
+// $(document).ready(function() {
+//     setTimeout(()=>{
+//         $('#Preloader').addClass('loader');
+//     },1000);
+//     setTimeout(()=>{
+//         $('#Preloader').remove('loader');
+//     },500);
+//   })
 
 // preloader
 $(document).ready(function() {
-    setTimeout(()=>{
-        $('#Preloader').addClass('loader');
-    },1000);
-    setTimeout(()=>{
-        $('#Preloader').remove('loader');
-    },500);
-  })
+  setTimeout(()=>{
+      $('#Preloader').addClass('loader');
+  },300);
+  setTimeout(()=>{
+      $('#Preloader').remove('loader');
+  },150);
+})
+
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
+
+
+// =========================================================
+
