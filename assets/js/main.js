20fb6bec8a28937480a9303d38c92b09ac3b4737
Dnyanesh_Kolhe <dnyaneshkolhe1@gmail.com>
2023-07-24 12:06:50 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,19 +136,19 @@ if(GetTheme == "DARK"){
 }
 
 
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
+// const toastTrigger = document.getElementById('liveToastBtn')
+// const toastLiveExample = document.getElementById('liveToast')
 
-if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
-}
+// if (toastTrigger) {
+//   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+//   toastTrigger.addEventListener('click', () => {
+//     toastBootstrap.show()
+//   })
+// }
 
-window.onload = (event) => {
-  var toastLiveExample = document.getElementById('liveToast')
-  var toast = new bootstrap.Toast(toastLiveExample)
-  toast.show()
-}
+// window.onload = (event) => {
+//   var toastLiveExample = document.getElementById('liveToast')
+//   var toast = new bootstrap.Toast(toastLiveExample)
+//   toast.show()
+// }
 
